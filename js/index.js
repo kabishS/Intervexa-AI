4b0a639cbc22d12f4c4bbaeca1b5b071b068dee6
kabish <kabishvimala@gmail.com>
2026-08-22 20:24:50 +0530

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-   const form = document.getElementById("loginForm");
-
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-
-        // Redirect to home page
-        window.location.href = "./home.html";
-    });
